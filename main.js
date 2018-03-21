3ead39ea1bc7bc65fbb49d8fb352cd363cceee8b
venki <nagallivenky@gmail.com>
2018-03-21 11:10:33 +0530

    added json and js

---
+++ main.js
@@ -0,0 +1,18 @@
+//ajax call initiation
+function jsondata(file,callback){
+var xhr=new XMLHttpRequest();
+xhr.overrideMimeType("application/json");
+xhr.open("GET",file,true);
+xhr.onreadystatechange=function(){
+  if(xhr.readyState === 4 && xhr.status === 200){
+    callback(xhr.responseText);
+  }
+  };
+  xhr.send();
+}
+//function calling
+jsondata("data.json",function(text){
+let data=JSON.parse(text);
+console.log(data);
+
+})
